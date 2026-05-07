463d53ae4b0eb8fdac02afe829a01b8c000804f8
Ghars-Wiem <wiem.ghars@altavo.fr>
2026-05-07 05:53:37 +0100

    JPA util changes

--- src/main/java/com/example/gestioncollecteinfo/util/JPAUtil.java
+++ src/main/java/com/example/gestioncollecteinfo/util/JPAUtil.java
@@ -1,11 +1,26 @@
 package com.example.gestioncollecteinfo.util;
 
 import jakarta.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JPAUtil {
 
-    private static final EntityManagerFactory emf =
-            Persistence.createEntityManagerFactory("gestionPU");
+    private static final EntityManagerFactory emf = buildEntityManagerFactory();
+
+    private static EntityManagerFactory buildEntityManagerFactory() {
+        Map<String, String> props = new HashMap<>();
+
+        String dbUrl = System.getenv("DATABASE_URL");
+        String dbUser = System.getenv("DATABASE_USER");
+        String dbPassword = System.getenv("DATABASE_PASSWORD");
+
+        if (dbUrl != null) props.put("jakarta.persistence.jdbc.url", dbUrl);
+        if (dbUser != null) props.put("jakarta.persistence.jdbc.user", dbUser);
+        if (dbPassword != null) props.put("jakarta.persistence.jdbc.password", dbPassword);
+
+        return Persistence.createEntityManagerFactory("gestionPU", props);
+    }
 
     public static EntityManager getEntityManager() {
         return emf.createEntityManager();
